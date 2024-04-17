69942ade31592339f8c66c44cf85b7028e5a5398
vinod <alavalav4@gmail.com>
2024-04-17 22:50:53 +0100

    all

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PortfolioController;
+use App\Http\Controllers\DemoController;
 
 
 
@@ -9,5 +10,6 @@ use App\Http\Controllers\PortfolioController;
 Route::prefix('admin')->group(function () {
     Route::get('/my-portfolio', [PortfolioController::class, 'myportfolio']);
     Route::get('/reports', [PortfolioController::class, 'myportfolio']);
+    Route::get('/demo', [DemoController::class, 'demo']);
     // Add more routes here
 });
